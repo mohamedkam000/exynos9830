c9cee6d06df34aa30b3dbbe13aa10ef7bbd5f814
Muhammad Kamal <42623678+mohamedkam000@users.noreply.github.com>
2025-07-23 13:25:19 +0200

    Add files via upload

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -1,18 +1,110 @@
-// SPDX-License-Identifier: GPL-2.0
 #include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
+
+enum {
+	FLAG_DELETE = 0,
+	FLAG_REPLACE,
+};
+
+static char new_command_line[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_puts(m, saved_command_line);
+	seq_puts(m, new_command_line);
 	seq_putc(m, '\n');
 	return 0;
 }
 
+static int process_flag(int replace, const char *flag, const char *new_var)
+{
+	char *start_flag, *end_flag, *next_flag;
+	char *last_char = new_command_line + COMMAND_LINE_SIZE;
+	size_t rest_len, flag_len, cmd_len, var_len, nvar_len;
+	int ret = 0;
+
+	while ((start_flag = strnstr(new_command_line, flag, COMMAND_LINE_SIZE))) {
+		end_flag = strnchr(start_flag, last_char - start_flag, ' ');
+
+		if (end_flag > last_char)
+			end_flag = last_char;
+
+		cmd_len = strlen(new_command_line);
+		if (unlikely(cmd_len > COMMAND_LINE_SIZE))
+			break;
+
+		next_flag = end_flag + 1;
+		rest_len = (size_t)(last_char - end_flag);
+		flag_len = (size_t)(end_flag - start_flag);
+
+		if (replace) {
+			if (!new_var)
+				break;
+
+			nvar_len = strlen(new_var);
+			var_len = flag_len - strlen(flag);
+
+			if (nvar_len > var_len &&
+			    (cmd_len + (nvar_len - var_len)) > COMMAND_LINE_SIZE)
+				break;
+		}
+
+		if (rest_len)
+			memmove(start_flag, next_flag, rest_len);
+
+		memset(last_char - flag_len, '\0', flag_len);
+
+		ret++;
+
+		if (replace) {
+			cmd_len = strlen(new_command_line);
+			if (unlikely(cmd_len > COMMAND_LINE_SIZE))
+				break;
+
+			sprintf(new_command_line + cmd_len, " %s%s", flag, new_var);
+
+			break;
+		}
+	}
+
+	return ret;
+}
+
 static int __init proc_cmdline_init(void)
 {
+	memcpy(new_command_line, saved_command_line,
+		min((size_t)COMMAND_LINE_SIZE, strlen(saved_command_line)));
+
+	process_flag(FLAG_REPLACE, "androidboot.verifiedbootstate=", "green");
+	process_flag(FLAG_REPLACE, "androidboot.warranty_bit=", "0");
+	process_flag(FLAG_REPLACE, "androidboot.fmp_config=", "1");
+	process_flag(FLAG_REPLACE, "androidboot.bootloader=", "cloudripper-14.0-11170085");
+	process_flag(FLAG_REPLACE, "androidboot.em.did=", "0000000000000000");
+	process_flag(FLAG_REPLACE, "androidboot.ap_serial=", "0x000000000000");
+	process_flag(FLAG_REPLACE, "androidboot.asb=", "1");
+//	process_flag(FLAG_REPLACE, "androidboot.avb_version=", "2.0");
+	process_flag(FLAG_REPLACE, "androidboot.carrierid=", "BTE");
+	process_flag(FLAG_REPLACE, "androidboot.bootreason=", "unknown");
+	process_flag(FLAG_REPLACE, "androidboot.em.model=", "SM-G996B");
+	process_flag(FLAG_REPLACE, "androidboot.bore_cnt=", "0");
+	//process_flag(FLAG_REPLACE, "androidboot.hardware.revision=", "22");
+	process_flag(FLAG_REPLACE, "androidboot.hmac_mismatch=", "0");
+	process_flag(FLAG_REPLACE, "androidboot.serialno=", "00000000000");
+	//process_flag(FLAG_REPLACE, "androidboot.revision=", "22");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+	//process_flag(FLAG_REPLACE, "androidboot.=", "");
+
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;
 }
